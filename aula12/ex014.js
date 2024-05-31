22e0031412ef7bcc99a552728563f2fd13f31581
Igor Barreto Santos <igor_barreto2003@hotmail.com>
2024-05-31 02:26:06 -0300

    exercicio 13 e 14

---
+++ aula12/ex014.js
@@ -0,0 +1,9 @@
+var hora = 22
+console.log(`Agora sao ${hora} horas.`)
+if (hora <12){
+    console.log('Bom dia!!')
+} else if (hora <18){
+    console.log('Boa tarde!!')
+}else if (hora < 24){
+    console.log('Boa noite!!')
+}

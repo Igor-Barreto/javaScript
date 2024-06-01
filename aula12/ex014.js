27573d3f1d1af903791b5686f0e29489f75f9525
Igor Barreto Santos <igor_barreto2003@hotmail.com>
2024-06-01 02:41:56 -0300

    exercicio 14 e 15

--- aula12/ex014.js
+++ aula12/ex014.js
@@ -1,9 +1,12 @@
-var hora = 22
+var agora = new Date()
+var hora = agora.getHours ()
 console.log(`Agora sao ${hora} horas.`)
-if (hora <12){
+if(hora < 6){
+    console.log('Boa madrugada!!')
+} else if (hora <12){
     console.log('Bom dia!!')
 } else if (hora <18){
     console.log('Boa tarde!!')
-}else if (hora < 24){
+} else if (hora < 24){
     console.log('Boa noite!!')
 }
